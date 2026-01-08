05555bf5e8e74a8a127afee4d2fdfcc0849fc939
amitcoderhub <amitchauhan6599@gmail.com>
2026-01-08 21:46:20 +0530

    django day8

--- app/models.py
+++ app/models.py
@@ -4,13 +4,17 @@ from django.db import models
 
 
 class Topic(models.Model):
-    topic_name = models.CharField(max_length=100)
+    topic_name = models.CharField(max_length=100, unique=True)
+    desc = models.CharField(max_length=100, null= True)
+    created_at = models.DateTimeField(null=True)
 
 
 class Webpage(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     url = models.URLField()
+    s_name= models.CharField(max_length=100, null= True)
+    sort = models.IntegerField(null=True)
 
 
 class AccessRecord(models.Model):
