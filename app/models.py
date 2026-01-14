c71ea637d0361286884cfe2d63ec8b6c0c341ad0
amitcoderhub <amitchauhan6599@gmail.com>
2026-01-14 14:24:29 +0530

    DONE

--- app/models.py
+++ app/models.py
@@ -1,30 +1,22 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Topic(models.Model):
-    topic_name = models.CharField(max_length=100, unique=True)
-    # desc = models.CharField(max_length=100, null= True)
-    
-    
+    topic_name = models.CharField(max_length=100, primary_key=True)
+
     def __str__(self):
         return self.topic_name
-    
+
 
 class Webpage(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     url = models.URLField()
-    s_name= models.CharField(max_length=100, null= True)
-    sort = models.IntegerField(null=True)
-    
+
     def __str__(self):
         return self.name
 
 
 class AccessRecord(models.Model):
-    name = models.ForeignKey(Webpage, on_delete=models.CASCADE, null= True)
-    author = models.CharField(max_length=100, null= True)
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE, null=True)
+    author = models.CharField(max_length=100, null=True)
     date = models.DateField()
-    
